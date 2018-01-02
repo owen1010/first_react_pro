75b1f57dea97a7c700ad49759dc39935a84c1a23
Ze Wilsen <wilsem_ze@outlook.com>
2018-01-02 21:21:28 +0800

    What's the render!

---
+++ render/render.js
@@ -0,0 +1,15 @@
+//render.js
+function tick(){
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>Time: {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );    
+}
+
+setInterval(tick, 1000);
